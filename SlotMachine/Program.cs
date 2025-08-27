cd51a61d4ff8d12a4f026c5c72f24b093c9f43ec
fedewagner <federico.j.wagner@gmail.com>
2025-08-27 20:10:52 +0200

    structure is commented

---
+++ SlotMachine/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SlotMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+            Design a game where the user can play a make-believe slot machine. 
+            The user will be asked to make a wager to play various lines in a 3 x 3 grid. 
+            They can play center line, all three horizontal lines, all vertical lines and diagonals.
+            For instance the user can enter $3 dollars and play all three horizontal lines. 
+            If the top line hits a winning combination, they earn $1 dollar for that line.
+        */
+            
+            //create variable to store user credit
+            //give info to user
+            //add credit option for the user
+            //make and show an empty grid
+            //create horizontal method
+            //create vertical method
+            //create diagonal method
+            //select the bet 1L = 1$, 3L = 3$, 6L = 8$ or 8L = 12$
+            //feed randomly the grid with the values
+            //check all the combinations
+            //check the bet and if the user won money
+            //display prize
+            //calculate new credit
+            //check out money option
+            //insert more money option
+            
+            
+        }
+    }
+}
